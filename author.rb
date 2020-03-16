b2c10098d7ee5d849d6f965da483c62752ad3d6b
duruchima <duruchima1@gmail.com>
2020-03-16 15:41:33 -0400

    all deliverables completed

--- author.rb
+++ author.rb
@@ -1,1 +1,54 @@
+class Author 
+@@all = []
+attr_accessor 
+attr_reader :name
+def initialize(name)
+    @name = name
+    Author.all << self
+end
 
+def books
+    Book.all.select {|book| book.author == self}
+end
+
+def write_book(title, word_count)
+    Book.new(title, self, word_count)
+end
+
+def total_words
+    total = 0
+    books.map {|books| total += books.word_count}
+    total
+end
+
+def self.most_words
+    most = 0
+    top_writer = ""
+    self.all.each do |authors|
+        words = authors.total_words
+        if words > most
+            most = words
+            top_writer = authors.name
+        end
+    end
+    top_writer
+end
+
+def self.least_words
+    least = 1000
+    most_concise_writer = ""
+    self.all.each do |authors|
+        words = authors.total_words
+        if words < least
+            least = words
+            most_concise_writer = authors.name
+        end
+    end
+    most_concise_writer
+end    
+
+def self.all
+    @@all
+end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,20 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+duncan = Author.new("Duncan Uruchima")
+stephanie = Author.new("Stephanie Reis")
+josh = Author.new("Josh McKay")
+alba = Author.new("Alba King")
+
+duncan.write_book('test', 5000)
+duncan.write_book('yes you can', 1000)
+duncan.write_book('believe in the me that believes in you', 75000)
+stephanie.write_book('freud and you', 9500)
+stephanie.write_book('how to psychoanalyze', 2500)
+alba.write_book('how to anxiety', 100)
+josh.write_book('babies!', 10)
+
+
 binding.pry
 
 
